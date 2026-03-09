a7ded38b456150fd75ed7c921198099335bf180d
aiitgenics1-netizen <ai.itgenics1@gmail.com>
2026-03-09 16:02:50 +0500

    Update ALLOWED_HOSTS to allow all hosts

--- review_monitor/settings.py
+++ review_monitor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3kw=920!)*6e*^7=*ag+6-d=6%3cyl%ms!$yf(grn$gg9!o(j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
